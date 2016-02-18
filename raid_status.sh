ece30396bbcf6099de26f6cc4a23dd68cb6a92e6
Marcus <me@marcusbean.com>
2016-02-17 23:35:43 -0600

    Commented out raid ok mail in raid status script

--- raid_status.sh
+++ raid_status.sh
@@ -2,8 +2,8 @@
 status=$(dmraid -s | grep status | awk '{print $3}')
 if [ ${status} != "ok" ]; then
 	/sbin/dmraid -s | mail -s "RAID Error on `hostname` : ${status}" me@marcusbean.com
-else
-	/sbin/dmraid -s | mail -s "RAID Ok on `hostname` : ${status}" me@marcusbean.com
+# else
+# 	/sbin/dmraid -s | mail -s "RAID Ok on `hostname` : ${status}" me@marcusbean.com
 fi
 
 exit 0
